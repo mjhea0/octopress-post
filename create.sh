7243473bd909c1a007c5c9dc3c11c49f4fa988d1
Michael Herman <hermanmu@gmail.com>
2013-10-11 14:56:47 -0600

    Update create.sh

--- create.sh
+++ create.sh
@@ -9,3 +9,19 @@ rake generate && rake preview
 
 
 work in progress
+
+
+
+#! /bin/bash
+
+BLOG_DIR=~/Dropbox/Projects/octopress
+cd $BLOG_DIR
+NEW_POST_PATH=`rake new_post["$1"] | awk '/Creating new post:/ {print $4}'`
+echo "Editing blog entry $BLOG_DIR/$NEW_POST_PATH"
+sleep 1
+vim $BLOG_DIR/$NEW_POST_PATH
+rake generate
+git add .
+git commit -m"$1"
+git push origin source
+rake deploy
